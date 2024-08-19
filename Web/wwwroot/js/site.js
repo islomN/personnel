8730c15b7595ebe6cc47d3f750f23199e2dfcbfc
Islom <nurmanovig@gmail.com>
2024-08-19 06:04:56 +0500

    Init

---
+++ Web/wwwroot/js/site.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function() {
+    $('#import').on('change', function(event) {
+        event.preventDefault();
+
+        let formData = new FormData(this);
+
+        $("#loading").show();
+        $.ajax({
+            url: '/Personnel/Import/',
+            type: 'POST',
+            data: formData,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                alert("Success count:" + response.count);
+                location.reload();
+            },
+            error: function(error) {
+                alert("Error message:" + error.responseJSON.message);
+            },
+            complete: function(){
+                $("#loading").hide();
+            }
+        });
+    });
+});
